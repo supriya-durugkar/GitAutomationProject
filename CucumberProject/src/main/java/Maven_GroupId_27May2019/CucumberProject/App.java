d72dbc8df8425f08bf0799afe5d0bdfa76ee1e77
RiteshDurugkar <RiteshDurugkar@LAPTOP-B4ELI0JD>
2019-11-21 22:51:03 +0530

    first commit

---
+++ CucumberProject/src/main/java/Maven_GroupId_27May2019/CucumberProject/App.java
@@ -0,0 +1,13 @@
+package Maven_GroupId_27May2019.CucumberProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
